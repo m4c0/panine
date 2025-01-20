494ba33520d9d3a74e0ba021ee6ccca9f4e81565
Eduardo Costa <m4c0@users.noreply.github.com>
2025-01-20 18:43:40 +0000

    splitting audio into module

--- viewer.cpp
+++ viewer.cpp
@@ -1,40 +1,16 @@
 #pragma leco app
 
+import audio;
 import casein;
-import jojo;
-import jute;
-import ovo;
-import siaudio;
-import silog;
 import sitime;
 import voo;
 
 static constexpr const auto window_height = 800;
 static constexpr const auto window_width = window_height * 9 / 16;
-static constexpr const auto audio_rate = 48000; // TODO: read from file
-
-static ovo::file audio = ovo::open_file("out/audio.ogg");
-static double audio_time = ovo::time_total(audio, 0);
-static int audio_bs {};
-
-static void audio_filler(float * data, unsigned samples) {
-  float ** pcm {};
-  while (samples > 0) {
-    auto r = ovo::read_float(audio, &pcm, samples, &audio_bs);
-    if (r <= 0) {
-      silog::log(silog::info, "Audio ended with code %ld", r);
-      siaudio::rate(0);
-      return;
-    }
-    for (int i = 0; i < r; i++) *data++ = pcm[0][i];
-    samples -= r;
-  }
-}
 
 struct init : public voo::casein_thread {
   init() {
     casein::window_size = { window_width, window_height };
-    siaudio::filler(audio_filler);
   }
 
   void run() {
@@ -44,7 +20,7 @@ struct init : public voo::casein_thread {
     main_loop("panine", [&](auto & dq, auto & sw) {
       ots_loop(dq, sw, [&](auto cb) {
         if (!started) {
-          siaudio::rate(audio_rate);
+          audio::start();
           timer = {};
           started = true;
         }
