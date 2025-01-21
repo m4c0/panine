b336c56030c244a9c4e7d812bb36498a926e2cf0
Eduardo Costa <m4c0@users.noreply.github.com>
2025-01-21 21:58:07 +0000

    buffer storage

--- main.frag
+++ main.frag
@@ -6,6 +6,13 @@ layout(push_constant) uniform upc {
 
 layout(set = 0, binding = 0) uniform sampler2D atlas;
 
+struct s_chr {
+  vec2 pos;
+  vec2 size;
+  vec4 uv;
+};
+layout(set = 1, binding = 0) readonly buffer b_chrs { s_chr cs[]; };
+
 layout(location = 0) in vec2 pos;
 
 layout(location = 0) out vec4 colour;
--- viewer.cpp
+++ viewer.cpp
@@ -61,8 +61,13 @@ struct init : public voo::casein_thread {
 
       vee::unmap_memory(*v_mem);
 
+      auto dsl_s = vee::create_descriptor_set_layout({ vee::dsl_fragment_storage() });
+      auto dpool_s = vee::create_descriptor_pool(1, { vee::storage_buffer() });
+      auto dset = vee::allocate_descriptor_set(*dpool_s, *dsl_s);
+      vee::update_descriptor_set_with_storage(dset, 0, *v_buf);
+
       vee::pipeline_layout pl = vee::create_pipeline_layout(
-          { *dsl },
+          { *dsl, *dsl_s },
           { vee::vert_frag_push_constant_range<upc>() });
 
       voo::one_quad_render oqr { "main", &dq, *pl };
@@ -88,6 +93,7 @@ struct init : public voo::casein_thread {
           oqr.run(*pcb, sw.extent(), [&] {
             vee::cmd_push_vert_frag_constants(*pcb, *pl, &pc);
             vee::cmd_bind_descriptor_set(*pcb, *pl, 0, scr.descriptor_set());
+            vee::cmd_bind_descriptor_set(*pcb, *pl, 1, dset);
           });
         });
       });
