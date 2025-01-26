30fbba689b8ebcb517fec67b57a2429bc4a63557
Eduardo Costa <m4c0@users.noreply.github.com>
2025-01-26 01:06:16 +0000

    refactor scriber code into a module

---
+++ scriber.cppm
@@ -0,0 +1,63 @@
+export module scriber;
+import dotz;
+import jute;
+import vee;
+import voo;
+import vtw;
+import what_the_font;
+
+struct chr {
+  dotz::vec2 pos;
+  dotz::vec2 size;
+  dotz::vec4 uv;
+};
+
+export class scriber {
+  static constexpr const auto max_chars = 16;
+
+  voo::host_buffer m_chrs;
+  vee::descriptor_set_layout m_dsl;
+  vee::descriptor_pool m_dpool;
+  vtw::scriber m_scr;
+
+public:
+  scriber(const voo::device_and_queue & dq, dotz::vec2 ext);
+
+  [[nodiscard]] auto cbuf() const { return m_chrs.buffer(); }
+  [[nodiscard]] auto dsl() const { return *m_dsl; }
+  [[nodiscard]] auto dset() const { return m_scr.descriptor_set(); }
+
+  void setup_copy(vee::command_buffer cb) { m_scr.setup_copy(cb); }
+
+  void shape(int scr_w, jute::view txt);
+};
+
+module : private;
+
+scriber::scriber(const voo::device_and_queue & dq, dotz::vec2 ext)
+  : m_chrs { dq, sizeof(chr) * max_chars }
+  , m_dsl { vee::create_descriptor_set_layout({ vee::dsl_fragment_sampler() }) }
+  , m_dpool { vee::create_descriptor_pool(1, { vee::combined_image_sampler() }) }
+  , m_scr { dq.physical_device(), vee::allocate_descriptor_set(*m_dpool, *m_dsl) } {
+  m_scr.bounds(ext);
+}
+
+void scriber::shape(int scr_w, jute::view txt) {
+  static constexpr const auto font_h = 128;
+  static wtf::library g_library{};
+  static wtf::face g_face = g_library.new_face("out/font.ttf");
+  g_face.set_char_size(font_h);
+
+  voo::mapmem mem { m_chrs.memory() };
+  auto vs = static_cast<chr *>(*mem);
+
+  auto s = g_face.shape_en(txt);
+  m_scr.pen((-s.bounding_box() + scr_w) / 2);
+  m_scr.draw(s, [&](auto pen, const auto & glyph) {
+    *vs++ = chr {
+      .pos  = pen + glyph.d,
+      .size = glyph.size,
+      .uv   = glyph.uv
+    };
+  });
+}
--- viewer.cpp
+++ viewer.cpp
@@ -4,38 +4,25 @@
 
 import audio;
 import casein;
-import dotz;
 import ofs;
+import scriber;
 import sdf_texture;
 import sitime;
 import vapp;
 import vee;
 import voo;
 import vtw;
-import what_the_font;
 
 static constexpr const auto window_height = 800;
 static constexpr const auto window_width = window_height * 9 / 16;
 
-static constexpr const auto font_h = 128;
-static constexpr const auto max_chars = 16;
-
-static wtf::library g_library{};
-static wtf::face g_face = g_library.new_face("out/font.ttf");
-
 struct upc {
   float aspect;
-};
-struct chr {
-  dotz::vec2 pos;
-  dotz::vec2 size;
-  dotz::vec4 uv;
 };
 
 struct init : public vapp {
   init() {
     casein::window_size = { window_width, window_height };
-    g_face.set_char_size(font_h);
   }
 
   void run() {
@@ -44,37 +31,17 @@ struct init : public vapp {
 
     main_loop("panine", [&](auto & dq, auto & sw) {
       ofs o { dq, { 1024, 1024 } };
-
-      auto dsl = vee::create_descriptor_set_layout({ vee::dsl_fragment_sampler() });
-      auto dpool = vee::create_descriptor_pool(1, { vee::combined_image_sampler() });
-      vtw::scriber scr { dq.physical_device(), vee::allocate_descriptor_set(*dpool, *dsl) };
-      scr.bounds({ 1024, 1024 });
+      scriber s { dq, { 1024, 1024 } };
 
       sdf_texture sdf { dq, { 1024, 1024 } };
-
-      auto chrs = voo::host_buffer { dq, sizeof(chr) * max_chars };
-      {
-        voo::mapmem mem { chrs.memory() };
-        auto vs = static_cast<chr *>(*mem);
-
-        auto s = g_face.shape_en("Engineering");
-        scr.pen((-s.bounding_box() + 1024) / 2);
-        scr.draw(s, [&](auto pen, const auto & glyph) {
-          *vs++ = chr {
-            .pos  = pen + glyph.d,
-            .size = glyph.size,
-            .uv   = glyph.uv
-          };
-        });
-      }
 
       auto dsl_s = vee::create_descriptor_set_layout({ vee::dsl_fragment_storage() });
       auto dpool_s = vee::create_descriptor_pool(1, { vee::storage_buffer() });
       auto dset = vee::allocate_descriptor_set(*dpool_s, *dsl_s);
-      vee::update_descriptor_set_with_storage(dset, 0, chrs.buffer());
+      vee::update_descriptor_set_with_storage(dset, 0, s.cbuf());
 
       vee::pipeline_layout pl = vee::create_pipeline_layout(
-          { *dsl, *dsl_s },
+          { s.dsl(), *dsl_s },
           { vee::vert_frag_push_constant_range<upc>() });
 
       voo::one_quad_render oqr { "main", &dq, *pl };
@@ -93,7 +60,8 @@ struct init : public vapp {
 
         sw.queue_one_time_submit(dq.queue(), [&](auto pcb) {
           if (!copied) {
-            scr.setup_copy(*pcb);
+            s.shape(1024, "Engineering");
+            s.setup_copy(*pcb);
             copied = true;
           }
 
@@ -103,7 +71,7 @@ struct init : public vapp {
           });
           oqr.run(*pcb, sw.extent(), [&] {
             vee::cmd_push_vert_frag_constants(*pcb, *pl, &pc);
-            vee::cmd_bind_descriptor_set(*pcb, *pl, 0, scr.descriptor_set());
+            vee::cmd_bind_descriptor_set(*pcb, *pl, 0, s.dset());
             vee::cmd_bind_descriptor_set(*pcb, *pl, 1, dset);
           });
         });
