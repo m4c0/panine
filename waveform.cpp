566276719398c2c229bd9ed1ad8570534dcf5834
Eduardo Costa <m4c0@users.noreply.github.com>
2025-02-03 11:33:08 +0000

    waveform: sampler

--- waveform.cpp
+++ waveform.cpp
@@ -10,10 +10,20 @@ import vapp;
 static struct : vapp {
   void run() override {
     main_loop("waveform", [&](auto & dq, auto & sw) {
-      auto pl = vee::create_pipeline_layout();
+      auto smp = vee::create_sampler(vee::linear_sampler);
+      voo::h2l_image img { dq.physical_device(), 1024, 1, vee::image_format_r8 };
+      voo::single_dset dset {
+        vee::dsl_fragment_sampler(),
+        vee::combined_image_sampler(),
+      };
+      vee::update_descriptor_set(dset.descriptor_set(), 0, img.iv(), *smp);
+
+      auto pl = vee::create_pipeline_layout({ dset.descriptor_set_layout() });
       voo::one_quad_render oqr { "waveform", &dq, *pl };
       ots_loop(dq, sw, [&](auto cb) {
-        oqr.run(cb, sw.extent());
+        oqr.run(cb, sw.extent(), [&] {
+          vee::cmd_bind_descriptor_set(cb, *pl, 0, dset.descriptor_set());
+        });
       });
     });
   }
--- waveform.frag
+++ waveform.frag
@@ -1,5 +1,7 @@
 #version 450
 
+layout(set = 0, binding = 0) uniform sampler2D buf;
+
 layout(location = 0) in vec2 pos;
 
 layout(location = 0) out vec4 colour;
