fad993d26695a89a97e535793e806e817cde85f7
Eduardo Costa <m4c0@users.noreply.github.com>
2025-02-19 21:31:16 +0000

    preparing to pass word control to AV

--- macspeech_osx.m
+++ macspeech_osx.m
@@ -1,15 +1,26 @@
 @import AVFoundation;
 
+@interface PNNSynth : AVSpeechSynthesizer<AVSpeechSynthesizerDelegate>
+@end
+@implementation PNNSynth
+- (void) speechSynthesizer:(AVSpeechSynthesizer *)synth willSpeakRangeOfSpeechString:(NSRange)range utterance:(AVSpeechUtterance *)utt {
+  NSString * buf = [utt.speechString substringWithRange:range];
+  NSLog(@"Will speak [%@]", buf);
+}
+@end
+
 void * ms_init() {
-  return (__bridge_retained void *)[[AVSpeechSynthesizer alloc] init];
+  PNNSynth * s = [PNNSynth new];
+  s.delegate = s;
+  return (__bridge_retained void *)s;
 }
 void ms_deinit(void * p) {
-  id n = (__bridge_transfer AVSpeechSynthesizer *)p;
+  id n = (__bridge_transfer PNNSynth *)p;
   NSLog(@"Deallocating %@", n);
 }
 
 int ms_playing(void * p) {
-  return [(__bridge AVSpeechSynthesizer *)p isSpeaking] ? 1 : 0;
+  return [(__bridge PNNSynth *)p isSpeaking] ? 1 : 0;
 }
 
 void ms_synth(void * p, void * w, int n) {
@@ -24,5 +35,5 @@ void ms_synth(void * p, void * w, int n) {
   utt.voice = [AVSpeechSynthesisVoice voiceWithIdentifier:@"com.apple.voice.compact.en-GB.Daniel"];
   utt.rate = (max - min) * 0.7 + min;
 
-  [(__bridge AVSpeechSynthesizer *)p speakUtterance:utt];
+  [(__bridge PNNSynth *)p speakUtterance:utt];
 }
