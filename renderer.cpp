cff6eed805db73f860062f3fb4482313d86a76be
Eduardo Costa <m4c0@users.noreply.github.com>
2025-02-22 19:24:50 +0000

    manual RGBA -> ARGB conversion

--- renderer.cpp
+++ renderer.cpp
@@ -7,14 +7,16 @@ import pipeline;
 import vee;
 import voo;
 
+struct pix { char p[4]; };
+
 static constexpr const vee::extent extent { 1080, 1920 };
-static constexpr const auto format = VK_FORMAT_A8B8G8R8_SRGB_PACK32;
+static constexpr const auto format = VK_FORMAT_R8G8B8A8_SRGB;
 
 extern "C" {
   void * vo_new(int w, int h);
   void vo_delete(void *);
   void vo_done(void *);
-  unsigned * vo_lock(void * p);
+  pix * vo_lock(void * p);
   void vo_unlock(void * p, unsigned frame);
   void vo_wait();
 }
@@ -46,10 +48,12 @@ int main() {
 
     {
       auto mm = fb.map_host();
-      auto * in = static_cast<unsigned *>(*mm);
-      auto * out = vo_lock(vo);
+      pix * in = static_cast<pix *>(*mm);
+      pix * out = vo_lock(vo);
       for (auto i = 0; i < extent.width * extent.height; i++) {
-        *out++ = *in++;
+        *out = {{ in->p[3], in->p[0], in->p[1], in->p[2] }};
+        out++;
+        in++;
       }
       vo_unlock(vo, 0);
     }
