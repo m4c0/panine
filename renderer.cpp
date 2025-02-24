6d0359c214cb458f3116cc9b65962466acd014ae
Eduardo Costa <m4c0@users.noreply.github.com>
2025-02-24 13:40:01 +0000

    moving speak code to another module

--- renderer.cpp
+++ renderer.cpp
@@ -1,15 +1,11 @@
 #pragma leco app
-#pragma leco add_impl avspeech
 
 import pipeline;
+import speak;
 import silog;
 import vee;
 import vo;
 import voo;
-
-import hai;
-import jojo;
-import jute;
 
 // static voo::device_and_queue dq { "panine-render" };
 
@@ -24,31 +20,10 @@ import jute;
 // 
 // static int i = 0;
 
-struct word {
-  jute::heap text;
-  unsigned offset;
-};
-
-static const auto script = jojo::read_cstr("out/script.txt");
-static hai::chain<float> buffer { 4 * 1024 * 1024 };
-static hai::chain<word> words { 16 * 1024 };
-
-extern "C" void speak(const char * txt, unsigned n);
-extern "C" void speak_callback(float * data, unsigned samples) {
-  for (auto i = 0; i < samples; i++) buffer.push_back(data[i]);
-}
-extern "C" void speak_marker_callback(const char * str, unsigned offset) {
-  if (str == nullptr) str = "";
-  words.push_back(word {
-    .text { jute::view::unsafe(str) },
-    .offset = offset / 4,
-  });
-}
-
 int main() {
-  speak(script.begin(), script.size());
-  for (auto & w : words) silog::trace(*w.text, w.offset);
-  silog::log(silog::info, "generated %d words and %d samples", words.size(), buffer.size());
+  spk::run();
+  for (auto & w : spk::words) silog::trace(*w.text, w.offset);
+  silog::log(silog::info, "generated %d words and %d samples", spk::words.size(), spk::buffer.size());
 
   //ms.write([](float * data, unsigned count) {
   //  silog::trace(count);
---
+++ speak.cppm
@@ -0,0 +1,34 @@
+#pragma leco add_impl avspeech
+export module speak;
+import hai;
+import jojo;
+import jute;
+
+extern "C" void speak(const char * txt, unsigned n);
+
+export namespace spk {
+  struct word {
+    jute::heap text;
+    unsigned offset;
+  };
+  
+  hai::chain<float> buffer { 4 * 1024 * 1024 };
+  hai::chain<word> words { 16 * 1024 };
+
+  void run() {
+    auto script = jojo::read_cstr("out/script.txt");
+    speak(script.begin(), script.size());
+  }
+}
+
+extern "C" void speak_callback(float * data, unsigned samples) {
+  for (auto i = 0; i < samples; i++) spk::buffer.push_back(data[i]);
+}
+extern "C" void speak_marker_callback(const char * str, unsigned offset) {
+  if (str == nullptr) str = "";
+  spk::words.push_back(spk::word {
+    .text { jute::view::unsafe(str) },
+    .offset = offset / 4,
+  });
+}
+
