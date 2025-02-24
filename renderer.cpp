4cf8b12c7e0dfaea9a2b90bee3b01904023629c1
Eduardo Costa <m4c0@users.noreply.github.com>
2025-02-24 13:34:16 +0000

    all important speech info properly stored

--- renderer.cpp
+++ renderer.cpp
@@ -7,6 +7,7 @@ import vee;
 import vo;
 import voo;
 
+import hai;
 import jojo;
 import jute;
 
@@ -23,18 +24,31 @@ import jute;
 // 
 // static int i = 0;
 
+struct word {
+  jute::heap text;
+  unsigned offset;
+};
+
 static const auto script = jojo::read_cstr("out/script.txt");
+static hai::chain<float> buffer { 4 * 1024 * 1024 };
+static hai::chain<word> words { 16 * 1024 };
 
 extern "C" void speak(const char * txt, unsigned n);
 extern "C" void speak_callback(float * data, unsigned samples) {
-  silog::trace(samples);
+  for (auto i = 0; i < samples; i++) buffer.push_back(data[i]);
 }
 extern "C" void speak_marker_callback(const char * str, unsigned offset) {
-  silog::trace(jute::view::unsafe(str), offset);
+  if (str == nullptr) str = "";
+  words.push_back(word {
+    .text { jute::view::unsafe(str) },
+    .offset = offset / 4,
+  });
 }
 
 int main() {
   speak(script.begin(), script.size());
+  for (auto & w : words) silog::trace(*w.text, w.offset);
+  silog::log(silog::info, "generated %d words and %d samples", words.size(), buffer.size());
 
   //ms.write([](float * data, unsigned count) {
   //  silog::trace(count);
