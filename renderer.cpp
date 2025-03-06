8e898165bf98a33e2b60b70b5db985f1cc54b0d4
Eduardo Costa <m4c0@users.noreply.github.com>
2025-03-06 16:23:58 +0000

    refactor to eventually render images

--- renderer.cpp
+++ renderer.cpp
@@ -13,13 +13,35 @@ static voo::device_and_queue dq { "panine-render" };
 static vo v {};
 static int vframes {};
 
+static constexpr const auto format = VK_FORMAT_R8G8B8A8_SRGB;
+static voo::offscreen::buffers fb { dq.physical_device(), vo::extent, format };
+static voo::single_cb cb { dq.queue_family() };
+static vee::render_pass_begin rpb = fb.render_pass_begin({});
+
+static void ots(auto fn) {
+  {
+    voo::cmd_buf_one_time_submit ots { cb.cb() };
+    fn();
+    fb.cmd_copy_to_host(cb.cb());
+  }
+  dq.queue()->queue_submit({
+    .command_buffer = cb.cb(),
+  });
+  vee::device_wait_idle();
+
+  auto mm = fb.map_host();
+  auto in = static_cast<vo::pix *>(*mm);
+  auto out = v.lock();
+  for (auto i = 0; i < vo::extent.width * vo::extent.height; i++) {
+    *out = {{ in->p[3], in->p[0], in->p[1], in->p[2] }};
+    out++;
+    in++;
+  }
+  v.unlock(vframes++);
+}
+
 static void run_speech(jute::view bg, jute::view script) {
-  constexpr const auto format = VK_FORMAT_R8G8B8A8_SRGB;
-  voo::offscreen::buffers fb { dq.physical_device(), vo::extent, format };
   pipeline ppl { bg, &dq, fb.render_pass(), false };
- 
-  voo::single_cb cb { dq.queue_family() };
-  vee::render_pass_begin rpb = fb.render_pass_begin({});
  
   auto spk = spk::run(script);
   v.write_audio(spk.buffer.begin(), spk.buffer.size());
@@ -29,32 +51,21 @@ static void run_speech(jute::view bg, jute::view script) {
     silog::trace("generate", w.text);
     auto count = w.offset * 30 / 22050;
     for (; frame < count; frame++) {
-      {
-        voo::cmd_buf_one_time_submit ots { cb.cb() };
+      ots([&] {
         ppl.run(cb.cb(), rpb, *w.text);
-        fb.cmd_copy_to_host(cb.cb());
-      }
-      dq.queue()->queue_submit({
-        .command_buffer = cb.cb(),
       });
       
-      vee::device_wait_idle();
-
-      {
-        auto mm = fb.map_host();
-        auto in = static_cast<vo::pix *>(*mm);
-        auto out = v.lock();
-        for (auto i = 0; i < vo::extent.width * vo::extent.height; i++) {
-          *out = {{ in->p[3], in->p[0], in->p[1], in->p[2] }};
-          out++;
-          in++;
-        }
-        v.unlock(vframes++);
-      }
-
       ppl.next_frame();
       vee::device_wait_idle();
     }
+  }
+}
+
+void show_image() {
+  // pipeline ppl { bg, &dq, fb.render_pass(), false };
+  for (int frame = 0; frame < 60; frame++) {
+    ots([&] {
+    });
   }
 }
 
